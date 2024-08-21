d3330aa061b33c66f8f7dcdeeff819ed9a94bc67
AshleyEvonne <ashleyevonne345@gmail.com>
2024-08-21 10:29:25 -0400

    Demostration of OneToOne relationships and Mapping

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,23 @@
+package org.example;
+
+import model.Address;
 import model.Department;
 import model.Teacher;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
 import java.util.ArrayList;
-import java.util.List;
+
+
 public class App {
     public static void main(String[] args) {
 
-        oneToMany();
+        oneToOne();
     }
 
-    public static void manyToOne(){
+    public static void manyToOne() {
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
         Transaction transaction = session.beginTransaction();
@@ -22,10 +27,10 @@ public class App {
         Department dept2 = new Department("HR");
 
         //creating teacher
-        Teacher t1 = new Teacher("1000","MHaseeb",dept1);
-        Teacher t2 = new Teacher("2220","Shahparan",dept1);
-        Teacher t3 = new Teacher("3000","James",dept1);
-        Teacher t4 = new Teacher("40000","Joseph",dept2);
+        Teacher t1 = new Teacher("1000", "MHaseeb", dept1);
+        Teacher t2 = new Teacher("2220", "Shahparan", dept1);
+        Teacher t3 = new Teacher("3000", "James", dept1);
+        Teacher t4 = new Teacher("40000", "Joseph", dept2);
 
         //Storing Departments in database
         session.persist(dept1);
@@ -38,16 +43,16 @@ public class App {
         transaction.commit();
     }
 
-    public static void oneToMany(){
+    public static void oneToMany() {
         SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
         Transaction t = session.beginTransaction();
         //creating teacher
-        Teacher t1 = new Teacher("1000","MHaseeb");
-        Teacher t2 = new Teacher("2220","Shahparan");
-        Teacher t3 = new Teacher("3000","James");
-        Teacher t4 = new Teacher("40000","Joseph");
-        Teacher t5 = new Teacher("200","Ali");
+        Teacher t1 = new Teacher("1000", "MHaseeb");
+        Teacher t2 = new Teacher("2220", "Shahparan");
+        Teacher t3 = new Teacher("3000", "James");
+        Teacher t4 = new Teacher("40000", "Joseph");
+        Teacher t5 = new Teacher("200", "Ali");
 
         //Add teacher entity object to the list
         ArrayList<Teacher> teachersList = new ArrayList<>();
@@ -67,11 +72,34 @@ public class App {
         department.setTeacherList(teachersList);
         //Storing Department
         session.persist(department);
-        t.commit();    }
+        t.commit();
+    }
 
+
+    public static void oneToOne() {
+        System.out.println("Maven + Hibernate + SQL One to One Mapping Annotations");
+
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        Transaction t = session.beginTransaction();
+        Address a1 = new Address("27th street", "NYC", "NY", 11103);
+        Address a2 = new Address("28th street", "Buffalo", "NY", 15803);
+
+        Teacher t1 = new Teacher("1000", "MHaseeb");
+        Teacher t2 = new Teacher("2220", "Shahparan");
+        t1.setAddress(a1);
+        t2.setAddress(a2);
+
+
+
+        session.persist(a1);
+        session.persist(a2);
+        session.persist(t1);
+        session.persist(t2);
+
+        t.commit();
+    }
 
 }
-
-
-
 
